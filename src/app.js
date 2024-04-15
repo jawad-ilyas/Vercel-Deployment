ef28c3e95b82c0c8730212c08a5302c55f2c4ca4
jawad ilyas <mughaljawad12@gmail.com>
2024-04-16 01:56:42 +0500

    changing into  index .js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
     res.status(200).send("/ is listening");
 });
 
-
+app.get("/api/v1", (req, res) => {
+    res.status(200).send("request to api version for deployments");
+});
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import cors from "cors";
-import app from "./app";
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).send("/ is listening");
+});
+
+app.get("/api/v1", (req, res) => {
+    res.status(200).send("request to api version for deployments");
+});
+
+export default app;
+
 
 app.use(cors({
     origin: "*",
