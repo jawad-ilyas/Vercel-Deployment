66db2766d0a29e514a75e273c0f9f12b925df961
jawad ilyas <mughaljawad12@gmail.com>
2024-04-15 23:57:10 +0500

    new add router

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ app.get("/", (req, res) => {
     res.status(200).send("/ is listen ")
 
 })
+app.get("/api/v1", (req, res) => {
+    res.status(200).send("requrest to api version for deployments ")
+
+})
