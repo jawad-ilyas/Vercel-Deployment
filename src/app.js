647aef0ea072692b15fb2487b3b3f0befa09403c
jawad ilyas <mughaljawad12@gmail.com>
2024-04-16 01:55:31 +0500

    changing into app or index. js

--- src/app.js
+++ src/app.js
@@ -6,8 +6,6 @@ app.get("/", (req, res) => {
     res.status(200).send("/ is listening");
 });
 
-app.get("/api/v1", (req, res) => {
-    res.status(200).send("request to api version for deployments");
-});
+
 
 export default app;
